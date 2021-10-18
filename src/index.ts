5e360e2b66e05ecf5ca3743e92c43c15835fee50
Ingrid <silva_ingrid@outlook.com>
2021-10-18 19:06:54 -0300

    Código finalizado

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,16 @@ enum CLASS_TYPE {
     NOTURNO = "noturno"
 }
 
+enum SPECIALTY {
+    REACT=1,
+    REDUX=2,
+    CSS=3,
+    TESTES=4,
+    TYPESCRIPT=5,
+    POO=6,
+    BACKEND=7
+}
+
 type createClassInput = {
  id: number ,
 name: string ,
@@ -36,6 +46,34 @@ endDate: string ,
 module:  number,
 type: CLASS_TYPE
 }
+
+type createStudentInput = {
+    id: number ,
+   name: string ,
+   email: string,
+   birthDate: string ,
+   hobbies: string [],
+   class_id: number
+   }
+
+   type createTeacherInput = {
+    id: number ,
+   name: string ,
+   email: string,
+   birthDate: string ,
+   specialty: SPECIALTY [],
+   class_id: number
+   }
+
+   type updateStudentInput ={
+student_id: number,
+class_id: number
+   }
+
+   type updateTeacherInput ={
+    teacher_id: number,
+    class_id: number
+       }
 
 app.post ("/class", async (req: Request, res: Response)=>{
     let errorCode = 400;
@@ -72,13 +110,199 @@ type: req.body.type
                 "${input.module}"
             )
             `)
-        res.status(201).send({ message:"Tumar criada com sucesso."})
+        res.status(201).send({ message:"Turma criada com sucesso."})
 
 
     }catch (error) {
         res.status(errorCode).send({message: error})
     }
 })
+
+app.post ("/student", async (req: Request, res: Response)=>{
+let errorCode = 400
+try {
+    const input: createStudentInput = {
+        id: req.body.id ,
+       name: req.body.name ,
+       email: req.body.email,
+       birthDate: req.body.birthDate ,
+       hobbies: req.body.hobbies,
+       class_id: req.body.class_id
+    }
+
+    if(!input.id|| !input.name || !input.birthDate
+        || input.hobbies.length>1 ){
+            errorCode = 422
+            throw new Error("Preencha os campos corretamente.")
+        }
+
+        await connection.raw(`
+        ISERT INTO STUDENT(id, name, email, birthDate, class_id)
+        VALUES (
+            ${input.id},
+            "${input.name}",
+            "${input.email}",
+            "${input.birthDate}",
+            "${input.class_id}"
+        );
+
+        `)
+
+        for (let hobby of input.hobbies){
+            const idHobby = Math.floor(Math.random()*1000000)
+            await connection.raw(`
+            ISENT INTO HOBBY(id, name)
+            VALUES(
+                ${idHobby},
+                "${hobby}"
+            )
+            `)
+
+            await connection.raw(`
+            ISENT INTO STUDENT_HOBBY(student_id, hobby_id)
+            VALUES(
+                ${input.id},
+                ${idHobby}
+            )
+            `)
+            
+        }
+
+        res.status(201).send({message: "Criado com sucesso"})
+
+}catch (error) {
+    res.status(errorCode).send({message: error})
+}
+
+})
+
+app.put("/student", async (req: Request, res: Response)=>{
+  let erroCode =400;
+  try {
+
+    const input: updateStudentInput = {
+        student_id: req.body.student_id,
+        class_id: req.body.class_id
+    }
+
+    await connection.raw (`
+    UPDATE STUDENT 
+    SET class_id = ${input.class_id}
+    WHERE id = ${input.student_id}
+    `)
+
+    res.status(200).send({message: "Atualizado com sucesso!"})
+
+  }  catch (error) {
+      res.status(erroCode).send({message: error})
+  }
+})
+
+app.get("/student/:id", async (req: Request, res: Response)=>{
+   let errorCode = 400
+    try {
+
+        const id = req.params.id
+
+        if (isNaN(Number(id))){
+            throw new Error ("Apenas valores numéricos")
+        }
+
+        const result = await connection.raw(`
+        SELECT ROUND (DATEDIFF("2021-01-01", birthDate)/365)
+        FROM STUDENT
+        WHERE id ${id}
+        `)
+
+        if (result[0].length === 0 ){
+            errorCode = 404
+            throw new Error("Não encontrado")
+        }
+
+        res.status(200).send({student: result [0] [0]})
+    }  catch (error) {
+        res.status(errorCode).send({message: error})
+    }
+
+})
+
+app.post ("/teacher", async (req: Request, res: Response)=>{
+    let errorCode = 400
+    try {
+        const input: createTeacherInput = {
+            id: req.body.id ,
+           name: req.body.name ,
+           email: req.body.email,
+           birthDate: req.body.birthDate ,
+           specialty: req.body.specialty,
+           class_id: req.body.class_id
+        }
+        if(!input.id|| !input.name || !input.birthDate
+            || input.specialty.length>1 ){
+                errorCode = 422
+                throw new Error("Preencha os campos corretamente.")
+            }
+
+            await connection.raw(`
+            ISERT INTO TEACHER(id, name, email, birthDate, class_id)
+            VALUES (
+                ${input.id},
+                "${input.name}",
+                "${input.email}",
+                "${input.birthDate}",
+                "${input.class_id}"
+            );
+    
+            `)
+    
+            for (let specialty of input.specialty){
+               
+    
+                await connection.raw(`
+                ISENT INTO STUDENT_HOBBY(student_id, hobby_id)
+                VALUES(
+                    ${input.id},
+                    ${SPECIALTY[specialty]}
+                )
+                `)
+                
+            }
+    
+            res.status(201).send({message: "Criado com sucesso"})
+    
+    
+
+    } catch (error) {
+        res.status(errorCode).send({message: error})
+    }
+    
+
+})
+    
+app.put("/teacher", async (req: Request, res: Response)=>{
+
+    let erroCode =400;
+  try {
+
+    const input: updateTeacherInput = {
+        teacher_id: req.body.teacher_id,
+        class_id: req.body.class_id
+    }
+
+    await connection.raw (`
+    UPDATE TEACHER 
+    SET class_id = ${input.class_id}
+    WHERE id = ${input.teacher_id}
+    `)
+
+    res.status(200).send({message: "Atualizado com sucesso!"})
+}  catch (error) {
+
+    res.status(erroCode).send({message: error})
+}
+})
+    
+        
 
 const server = app.listen(process.env.PORT || 3003, () => {
     if (server) {
